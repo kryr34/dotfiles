a5d8d6562b373252f7e80ac2d5a6452adbd18454
kryr34 <kryr34@proton.me>
2024-06-27 02:30:09 +0800

    [shell] unfxck up & minor

--- shell/init.zsh
+++ shell/init.zsh
@@ -8,7 +8,7 @@ function _fishy_collapsed_wd() {
             if [[ "$pwd[$i]" = .* ]]; then
                 pwd[$i]="${${pwd[$i]}[1,3]}"
             else
-                pwd[$i]="${${pwd[$i]}[1]}"
+                pwd[$i]="${${pwd[$i]}[1,2]}"
             fi
         done
     fi
@@ -20,4 +20,7 @@ PS1=' ${CUSTOM_MSG} $(_fishy_collapsed_wd) %F{green}$%f '
 local return_status="%B%F{red}%(?..%?)%f%b"
 RPROMPT="${return_status} %T"
 # end o prompt
+
+# enable emacs mode
+bindkey -e
 
