616b72b59e81a6e59fa895900b52682312f91c38
kryr34 <kryr34@proton.me>
2024-07-10 11:07:48 +0800

    remove shell bs

--- shell/default.nix
+++ shell/default.nix
@@ -5,12 +5,12 @@ let
   historySave = 50000; # in file
 in
 {
-#  programs.bash.historyFile = historyFile;
-#  programs.bash.historySize = historySize;
-#  programs.bash.historyFileSize = historySave;
-#  programs.zsh.history.path = historyFile;
-#  programs.zsh.history.size = historySize;
-#  programs.zsh.history.save = historySave;
+  #  programs.bash.historyFile = historyFile;
+  #  programs.bash.historySize = historySize;
+  #  programs.bash.historyFileSize = historySave;
+  #  programs.zsh.history.path = historyFile;
+  #  programs.zsh.history.size = historySize;
+  #  programs.zsh.history.save = historySave;
   programs.bash.initExtra = builtins.readFile ./init.bash;
   programs.zsh.initExtra = builtins.readFile ./init.zsh;
   home.shellAliases = {
